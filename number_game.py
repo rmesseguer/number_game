40977715cdd8f1905395b8bef2aa576ab245f8a0
Ruben Messeguer <rubenmesseguer@gmail.com>
2019-11-18 17:47:53 +0000

    updated function

--- number_game.py
+++ number_game.py
@@ -1,25 +1,30 @@
 import random
 
 
-times = 1
+
 robot_score = 0
 player_score = 0
 
 
 while True:
     num = random.randint(1,10)
-    try:
-        guess = int(input('Guess a number between 1 and 10: '))    
-    except ValueError:
-        guess = int(input("Sorry I didnt understand that. Please guess again: "))
-            
 
-    while guess != num:
-        if (guess > 10):
-            print('The number must be between 1 and 10')
-            times = times -1
-            
+    good_guess = False
 
+    while not good_guess:
+        try:
+            guess = int(input('Guess a number between 1 and 10: '))    
+            
+            if guess < 1 or guess > 10:
+                raise ValueError()
+
+            good_guess = True
+        except ValueError:
+            print("Sorry I didnt understand that. Please try again.")
+    
+    times = 1
+
+    while times < 3 and guess != num:
         if (guess > num):
             try:
                 guess = int(input('Lower. Please guess again: '))
@@ -31,13 +36,12 @@ while True:
                 guess = int(input('Higher. Guess again: '))
             except ValueError:
                 print("Sorry I didnt understand that. Please guess again:")
-                    
-
 
         times = times + 1
+
         if (times == 3):
-            break
-            print('You lose! The number was ' + str(num))
+            print('Too many tries!')
+            
             
 
     if (guess == num):
